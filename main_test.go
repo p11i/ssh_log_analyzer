d0a6c29113104a2c71454b3d0d12c3b2166f4632
p11i <pouyahosseyni7@gmail.com>
2024-01-29 22:39:19 +0330

    add test by username

--- main_test.go
+++ main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	analysis "p11i/ssh_log_analyzer/analysis"
 	parsing "p11i/ssh_log_analyzer/parsing"
+	"testing"
 	"time"
 )
 
@@ -12,4 +14,12 @@ var mockLogEntries = []parsing.LogEntry{
 	{Time: time.Now().Add(-1 * time.Hour), IPAddress: "192.168.1.3", Username: "user3", AttemptType: "password"},
 	{Time: time.Now().Add(-1 * time.Hour), IPAddress: "192.168.1.4", Username: "user4", AttemptType: "password"},
 	{Time: time.Now(), IPAddress: "192.168.1.5", Username: "user1", AttemptType: "publickey"},
+}
+
+func TestFilterResults(t *testing.T) {
+	// Test filtering by username
+	filteredUser := analysis.FilterResults(mockLogEntries, "user1", "", time.Now().Add(-3*time.Hour))
+	if len(filteredUser) != 2 {
+		t.Errorf("Expected 2 entries for user1, got %d", len(filteredUser))
+	}
 }
