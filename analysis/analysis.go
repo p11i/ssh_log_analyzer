540dd8e0d5b1bdf3ebc47c0410f32eae020ed9dc
p11i <pouyahosseyni7@gmail.com>
2024-01-29 21:13:12 +0330

    add filtering

--- analysis/analysis.go
+++ analysis/analysis.go
@@ -1,3 +1,17 @@
 // analysis/analysis.go
 
 package analysis
+
+import "time"
+
+func FilterResults(logEntries []parsing.LogEntry, filterUser, filterIP string, timeThreshold time.Time) []parsing.LogEntry {
+	var filteredEntries []parsing.LogEntry
+	for _, entry := range logEntries {
+		if (filterUser == "" || entry.Username == filterUser) &&
+			(filterIP == "" || entry.IPAddress == filterIP) &&
+			entry.Time.After(timeThreshold) {
+			filteredEntries = append(filteredEntries, entry)
+		}
+	}
+	return filteredEntries
+}
