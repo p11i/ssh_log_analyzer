eb4bd6f6882bbdb6d1233efa73ffad16d4fc47cd
p11i <pouyahosseyni7@gmail.com>
2024-01-29 21:09:11 +0330

    add cmd flags
    for filtering and providing path to custom log file

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 // main.go
 package main
 
+import (
+	"flag"
+	"time"
+)
+
 func main() {
+	// Command-line flags
+	logFile := flag.String("log", "/var/log/auth.log", "Path to the log file")
+	filterUser := flag.String("user", "", "Filter results by username")
+	filterIP := flag.String("ip", "", "Filter results by IP address")
+	timeRange := flag.Duration("time", 24*time.Hour, "Time range for analysis (e.g., 1h, 24h)")
+	flag.Parse()
 }
